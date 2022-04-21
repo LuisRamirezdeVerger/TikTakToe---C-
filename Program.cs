d413d49fb2f9e789e1ac79bbe18b265807afa617
Luis a.k.a. Wito <job.wito.uk@gmail.com>
2022-04-21 15:59:29 +0100

    Finished!!

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace TikTakToe
             int ingreso = 0;
             bool ingresoCorrecto = true;
 
-            CreaTablero();
+            
 
 
             do
@@ -18,17 +18,97 @@ namespace TikTakToe
                 if(jugador == 2){
                     jugador = 1;
                     PonerXoO(jugador, ingreso);//break; //Necesitamos los "breaks" para que el resto del codigo sea accesible
-                }else if (jugador == 1){
+                } else if (jugador == 1){
                     jugador = 2;
                     PonerXoO(jugador, ingreso);//break; Pero en este caso no son necesario porque tenemos un "do" anidado
 
                 }
 
+                CreaTablero();
+
+
+                //Verifica si hay un ganador
+                #region
+                char [] cadaSigno = {'X', 'O'};
+
+                foreach (char signo in cadaSigno)
+                {
+                    if((tableroJuego[0,0] == signo) && (tableroJuego[0,1] == signo) && (tableroJuego[0,2] == signo)
+                        || (tableroJuego[1,0] == signo) && (tableroJuego[1,1] == signo) && (tableroJuego[1,2] == signo)
+                        || (tableroJuego[2,0] == signo) && (tableroJuego[2,1] == signo) && (tableroJuego[2,2] == signo)
+                        || (tableroJuego[0,0] == signo) && (tableroJuego[1,0] == signo) && (tableroJuego[1,2] == signo)
+                        || (tableroJuego[0,1] == signo) && (tableroJuego[1,1] == signo) && (tableroJuego[2,1] == signo)
+                        || (tableroJuego[0,2] == signo) && (tableroJuego[1,2] == signo) && (tableroJuego[2,2] == signo)
+                        || (tableroJuego[0,0] == signo) && (tableroJuego[1,1] == signo) && (tableroJuego[2,2] == signo)
+                        || (tableroJuego[0,2] == signo) && (tableroJuego[1,1] == signo) && (tableroJuego[2,0] == signo))
+
+                        {
+                            if (signo == 'X')
+                                Console.WriteLine("GG! Jugador 1 WINS!");
+                            else 
+                                Console.WriteLine("GG! Jugador 2 WINS!");
+
+                        Console.WriteLine("Presione cualquier tecla para reiniciar el juego");
+                        Console.Read();
+                        ingreso = 0;
+                        Resetear();
+                        
+                        break;
+                            
+                        }
+                    else if(turnos == 10){
+                        Console.WriteLine("\nEmpate!");
+                        Console.WriteLine("Presione cualquier tecla para reiniciar el juego");
+                        Console.Read();
+                        Resetear();
+                        break;
+
+                    }
+                        
+                }
+                            
+                        
+              
+                #endregion
+
+                //Codigo que verifica si el valor ingresado es correcto
+                #region
                 do
                 {
                     Console.WriteLine("\nJugador {0}, elija su casilla...", jugador);
-                    ingreso = Convert.ToInt32(Console.ReadLine());//(Leer de derecha a izq) Lo que el usuario escriba lo convierte en un "int" y lo almacena en 'ingreso'
+                    try
+                    {
+                        ingreso = Convert.ToInt32(Console.ReadLine());//(Leer de derecha a izq) Lo que el usuario escriba lo convierte en un "int" y lo almacena en 'ingreso'
+
+                    }
+                    catch (System.Exception)
+                    {
+                        Console.WriteLine("Por favor, ingrese un number");
+                    }
                 
+                    if((ingreso == 1) && (tableroJuego[0,0] == '1')){
+                        ingresoCorrecto = true;
+                    } else if ((ingreso == 2) && (tableroJuego[0,1] == '2')){
+                        ingresoCorrecto = true;
+                    } else if ((ingreso == 3) && (tableroJuego[0,2] == '3')){
+                        ingresoCorrecto = true;
+                    } else if ((ingreso == 4) && (tableroJuego[1,0] == '4')){
+                        ingresoCorrecto = true;
+                    } else if ((ingreso == 5) && (tableroJuego[1,1] == '5')){
+                        ingresoCorrecto = true;
+                    } else if ((ingreso == 6) && (tableroJuego[1,2] == '6')){
+                        ingresoCorrecto = true;
+                    } else if ((ingreso == 7) && (tableroJuego[2,0] == '7')){
+                        ingresoCorrecto = true;
+                    } else if ((ingreso == 8) && (tableroJuego[2,1] == '8')){
+                        ingresoCorrecto = true;
+                    } else if ((ingreso == 9) && (tableroJuego[2,2] == '9')){
+                        ingresoCorrecto = true;
+                    } else {
+                        Console.WriteLine("\nIngreso incorrecto, elige otro numero");
+                        ingresoCorrecto = false;
+                    }
+                    #endregion
 
 
                 } while (!ingresoCorrecto);
@@ -45,9 +125,9 @@ namespace TikTakToe
             char signo = ' ';
 
             if(jugador == 1){
-                signo = 'X';
-            } else if(jugador == 2){
                 signo = 'O';
+            } else if(jugador == 2){
+                signo = 'X';
             }
 
             switch (ingreso)
@@ -73,9 +153,23 @@ namespace TikTakToe
 
         };
 
+        //Array que contiene variables del juego inicial
+        static char[,] tableroJuegoInicial = 
+        {
+            {'1', '2', '3'},
+            {'4', '5', '6'},
+            {'7', '8', '9'}
+
+        };
+
+
+        //Declarar empate
+        static int turnos = 0;
+
         //Metodo para crear el tablero
         public static void CreaTablero()
         {
+            Console.Clear(); // Limpia la pantalla, asi cada vez que haya un cambio, reflejara el cambio y no toooodas las lineas
             Console.WriteLine("     |     |");
             Console.WriteLine("  {0}  |  {1}  |  {2}",tableroJuego[0,0], tableroJuego[0,1], tableroJuego[0,2]);
             Console.WriteLine("_____|_____|_____");
@@ -85,8 +179,15 @@ namespace TikTakToe
             Console.WriteLine("     |     |");
             Console.WriteLine("  {0}  |  {1}  |  {2}",tableroJuego[2,0], tableroJuego[2,1], tableroJuego[2,2]);
             Console.WriteLine("     |     |");
+            turnos++;
 
 
+        }
+
+        public static void Resetear(){
+            tableroJuego = tableroJuegoInicial;
+            CreaTablero();
+            turnos = 0;
         }
     }
 }
